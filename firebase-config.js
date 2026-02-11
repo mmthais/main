d3c4f040f4d2181fd3d4cb30c75bfba0a81b670d
Thaís <106763682+mmthais@users.noreply.github.com>
2026-02-11 12:40:15 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,5 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Preenchido com o seu projeto Firebase
+export const firebaseConfig = {
   apiKey: "AIzaSyATm-VZArjMfEs1d6JmmuUM8a7a45HXLJg",
   authDomain: "checklist-a150c.firebaseapp.com",
   projectId: "checklist-a150c",
@@ -12,6 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "700616349614",
   appId: "1:700616349614:web:5c815f9ac91403c7cf5a3f"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
