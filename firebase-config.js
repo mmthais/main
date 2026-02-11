3f6c44152a6310ade3dcaec55b15cd2256f994e1
Thaís <106763682+mmthais@users.noreply.github.com>
2026-02-11 12:14:15 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,9 +6,20 @@
 //    adicione seu domínio do GitHub Pages (ex: seuusuario.github.io) e/ou seu domínio custom.
 // 6) Regras sugeridas (Firestore) estão no README.md
 
-export const firebaseConfig = {
-  apiKey: "COLE_AQUI",
-  authDomain: "COLE_AQUI",
-  projectId: "COLE_AQUI",
-  appId: "COLE_AQUI"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyATm-VZArjMfEs1d6JmmuUM8a7a45HXLJg",
+  authDomain: "checklist-a150c.firebaseapp.com",
+  projectId: "checklist-a150c",
+  storageBucket: "checklist-a150c.firebasestorage.app",
+  messagingSenderId: "700616349614",
+  appId: "1:700616349614:web:5c815f9ac91403c7cf5a3f"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
